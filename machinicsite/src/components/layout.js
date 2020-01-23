881580bce3a1409b6a76c2d4f372599bcc7f92ee
Jordan Peacock <hewhocutsdown@gmail.com>
2020-01-23 13:32:44 -0600

    Fixed footer link

--- machinicsite/src/components/layout.js
+++ machinicsite/src/components/layout.js
@@ -37,7 +37,7 @@ const Layout = ({ children }) => {
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a> | [a href="https://4c541dcc-3a2d-4091-9e5f-607c2ba4d855.filesusr.com/ugd/096995_5b7b44a5e3aa4cfda8ee50b98565d932.pdf">Privacy Policy</a>]
+          <a href="https://www.gatsbyjs.org">Gatsby</a> | <a href="https://4c541dcc-3a2d-4091-9e5f-607c2ba4d855.filesusr.com/ugd/096995_5b7b44a5e3aa4cfda8ee50b98565d932.pdf">Privacy Policy</a>
         </footer>
       </div>
     </>
