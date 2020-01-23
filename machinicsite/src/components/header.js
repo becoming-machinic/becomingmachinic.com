99270ea24c40f5075e9834b3b018ccfcbdb3d8b9
Jordan Peacock <hewhocutsdown@gmail.com>
2020-01-23 13:22:02 -0600

    added logo, decent patch up

--- machinicsite/src/components/header.js
+++ machinicsite/src/components/header.js
@@ -1,32 +1,23 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Image from "../components/image"
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#113340`,
+      background: `#e8ded2`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        maxWidth: 450,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <Image />
     </div>
   </header>
 )
