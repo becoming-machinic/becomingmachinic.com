61fe261229b9f2459c7a9482e4548ab80f5cbd95
Jordan Peacock <hewhocutsdown@gmail.com>
2020-01-23 12:55:30 -0600

    Fix header bg

--- machinicsite/src/components/header.js
+++ machinicsite/src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `113340`,
+      background: `#113340`,
       marginBottom: `1.45rem`,
     }}
   >
