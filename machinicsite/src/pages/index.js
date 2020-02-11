2fa70cec0634865eca2845808758522ac007eab3
Jordan Peacock <hewhocutsdown@gmail.com>
2020-02-10 20:32:46 -0600

    Fixed URL

--- machinicsite/src/pages/index.js
+++ machinicsite/src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
     <p>Our clients haven't cared about our minimalist web presence or lack of stock photography.</p>
     <p>What they <em>have</em> cared about is the money and time they've saved by working with us to integrate their existing IT infrastructure and business processes.</p>
     <p>Need to connect RPG applications on your IBM i to a cloud application? SCADA controllers and mobile applications? Data from your manufacturing floor into your ERP system?</p>
-    <p>Our solutions are tailored, not bespoke, and are available at <a href="https://machinic.io>machinic.io">machinic.io</a> or on-premise as a virtual appliance.</p>
+    <p>Our solutions are tailored, not bespoke, and are available at <a href="https://machinic.io">machinic.io</a> or on-premise as a virtual appliance.</p>
     <p>Call us at 1-612-428-0415, or email at <a href="mailto:sales@machinic.io">sales@machinic.io</a> and let's talk.</p>
   </Layout>
 )
